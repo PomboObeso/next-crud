4aa24e717359f7a47a13fa1215bb0351ed3f3642
PomboObeso <matheus.gomes201@gmail.com>
2025-10-28 10:28:48 -0300

    Componentes de Tabela + Classe de Clientes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 
 export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 32, '1'),
+    new Cliente('Carlos', 22, '2'),
+    new Cliente('Bia', 42, '3'),
+    new Cliente('Pedro', 62, '4')
+  ]
   return (
     <div className="flex h-screen justify-center items-center bg-linear-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   );
